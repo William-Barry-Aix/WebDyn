fc8dcbf86f54b8f924f71a6510f3cbdeeb777f6f
William <wiwi.barry@hotmail.fr>
2018-11-21 18:31:06 +0100

    mise en bloc de la liste des immages

--- src/index.php
+++ src/index.php
@@ -35,20 +35,13 @@ include 'View.php';
 				<p></p>	
 		</article>
 		
-		<aside class="images">
-			<h2>Images</h2>
-			<figure>
-				<img src="./public/img/movie1.jpg" alt="movie_photo_1" />
-			</figure>
-			<figure>
-				<img src="./public/img/movie2.jpg" alt="" />
-			</figure>
-		</aside>
 		</article>
 	</section>
 	<footer>
 		<h5>Barry William</h5>
 	</footer>
+				<?php getBlock('infos');?>
+				<?php getBlock('images');?>
 	</main>
 </body>
 </html>
