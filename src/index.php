2d6fca67a2c68556c64e0696f540c7471669255b
William <wiwi.barry@hotmail.fr>
2018-11-21 18:26:30 +0100

    mise en bloc du header

--- src/index.php
+++ src/index.php
@@ -10,17 +10,7 @@ include 'View.php';
 </head>
 <body>
 	<header>
-		<article class="description">
-			<figure class="headImage">
-				<img class="headImage" src="./public/img/movie.jpg" alt="La Forme de l'eau" />
-			</figure>
-			<div class="article_description">
-				<h1>La Forme de l'eau </h1>
-				<time datetime="2018-01-01 00:00">(2018)</time>
-				<h2>4/5</h2>
-				<meter min="0" max="5" value="4"/>
-			</div>
-		</article>
+		<?php getBlock('header');?>
 	</header>
 	<main>
 	<section>
