712c7e6f9955138dab5275fa1f34090e0d09958e
William <wiwi.barry@hotmail.fr>
2018-11-21 18:32:36 +0100

    mise en bloc des infos des acteurs

--- src/index.php
+++ src/index.php
@@ -17,29 +17,13 @@ include 'View.php';
 		
 		
 
-		<article class="actors_infos">
-			<h2>Actors Infos</h2>
-				<h3>Sally Hawkins</h3>
-				<p>Actrice britannique née le 27 avril 1976 à Dulwich.</p>
-
-				<h3>Michael Shannon</h3>
-				<p></p>
-
-				<h3>Richard Jenkins</h3>
-				<p></p>
-
-				<h3>Doug Jones</h3>
-				<p></p>
-
-				<h3>Michael Stuhlbarg</h3>
-				<p></p>	
-		</article>
 		
 		</article>
 	</section>
 	<footer>
 	</footer>
 				<?php getBlock('infos');?>
+				<?php getBlock('personsInfos'); ?>
 				<?php getBlock('images');?>
 			<?php getBlock('footer');?>
 	</main>
---
+++ src/personsInfos.php
@@ -0,0 +1,18 @@
+<article class="actors_infos">
+	<h2>Actors Infos</h2>
+	
+	<h3>Sally Hawkins</h3>
+	<p>Actrice britannique née le 27 avril 1976 à Dulwich.</p>
+
+	<h3>Michael Shannon</h3>
+	<p></p>
+
+	<h3>Richard Jenkins</h3>
+	<p></p>
+
+	<h3>Doug Jones</h3>
+	<p></p>
+
+	<h3>Michael Stuhlbarg</h3>
+	<p></p>	
+</article>
