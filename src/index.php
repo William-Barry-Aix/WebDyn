78a842f94423106a86ee4304a2d079a747e6055d
William <wiwi.barry@hotmail.fr>
2018-11-21 18:29:01 +0100

    mise en bloc des infos du film

--- src/index.php
+++ src/index.php
@@ -16,30 +16,6 @@ include 'View.php';
 	<section>
 		
 		
-		<article class="content">
-			<article class="real_infos">
-			<p>Film de <a href="./director.html">Guillermo del Toro</a></p>					
-		</article>
-		<article class="synopsis">
-			<h2>Synopsis</h2>
-			<p>Modeste employée d’un laboratoire gouvernemental ultrasecret, Elisa mène une existence solitaire, d’autant plus isolée qu’elle est muette. Sa vie bascule à jamais lorsqu’elle et sa collègue Zelda découvrent une expérience encore plus secrète que les autres…</p>
-		</article>
-		<article class="main_actors">
-			<h2>Acteurs Principaux</h2>
-			<figure>
-				<img src="./public/img/actor1.jpg" alt="Sally Hawkins" />
-				<figcaption>
-					<h3>Sally Hawkins</h3>
-				</figcaption>
-			</figure>
-			<figure>
-				<img src="./public/img/actor2.png" alt="Michael Shannon" />
-				<figcaption>
-					<h3>Michael Shannon</h3>
-				</figcaption>
-			</figure>
-			
-		</article>
 
 		<article class="actors_infos">
 			<h2>Actors Infos</h2>
