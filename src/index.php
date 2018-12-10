8549bdcb7b396186faea45a99b88ac6dc51f7ba7
William <wiwi.barry@hotmail.fr>
2018-12-10 08:26:58 +0100

    chargement des autoload

--- src/index.php
+++ src/index.php
@@ -1,7 +1,6 @@
 <?php
 require_once(__DIR__ . '/libs/autoload.php');
 require_once(__DIR__ . '/controller/autoload.php');
-spl_autoload_register(array('Bootstrap', 'autoload'));
-spl_autoload_register(array('BootstrapApp', 'autoload'));
-
+spl_autoload_register(array('Bootstrap', 'autoload'), true);
+spl_autoload_register(array('BootstrapApp', 'autoload'), true);
 Monframework\App\Url::init();
