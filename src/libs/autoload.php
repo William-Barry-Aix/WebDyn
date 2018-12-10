1bacce41c11fee85b592386751c1eb7ed975c52f
William <wiwi.barry@hotmail.fr>
2018-12-10 08:27:32 +0100

    utilisation d'autoload pour charger les fichier php

--- src/libs/autoload.php
+++ src/libs/autoload.php
@@ -3,10 +3,14 @@ class Bootstrap
 {
     public static function autoload($className)
     {
-        echo '<pre>Autoload : ' . $className;
+        //echo '<pre>Autoload : ' . $className;
         $tab = explode('\\', $className);
         $path = __DIR__ . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $tab) . '.php';
-        echo "\n    =&gt; $path</pre>";
-        require $path;
+        //echo "\n    =&gt; $path</pre>";
+        if (file_exists($path)){
+        	require $path;
+        } else {
+        	//echo '<pre>Not Found</pre>';
+        }
     }
 }
